5c11dc3b89021319075dc08f5cd6b7efb6284b13
Arthur Santos <arthurldsantos@gmail.com>
2025-08-05 16:57:44 -0300

    criar funcionalidade do envio de pensamentos para a api

--- js/api.js
+++ js/api.js
@@ -8,8 +8,23 @@ const api = {
             alert('Erro ao buscar pensamentos');
             throw error;
         }
+    },
+
+    async salvarPensamento(pensamento) {
+        try{
+            const response = await fetch('http://localhost:3000/pensamentos', {
+                method: 'POST',
+                headers: {
+                    'Content-type': 'application/json'
+                },
+                body: JSON.stringify(pensamento)
+            });
+            return await response.json();
+        }
+        catch{}
     }
 
 }
 
 export default api;
+
