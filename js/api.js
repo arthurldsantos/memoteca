c0b9da24ad0f9ed74b912be3ac83ffb380f0d639
Arthur Santos <arthurldsantos@gmail.com>
2026-03-06 19:05:00 -0300

    implementar outras requisicoes http com a biblioteca axios

--- js/api.js
+++ js/api.js
@@ -25,8 +25,8 @@ const api = {
 
     async buscarPensamentoPorId(id) {
         try {
-            const response = await fetch(`${URL_BASE}/pensamentos/${id}`);
-            return await response.json();
+            const response = await axios.get(`${URL_BASE}/pensamentos/${id}`);
+            return await response.data;
         }
         catch{
             alert('Erro ao buscar pensamento');
@@ -36,14 +36,8 @@ const api = {
 
     async editarPensamento(pensamento) {
         try{
-            const response = await fetch(`${URL_BASE}/pensamentos/${pensamento.id}`, {
-                method: 'PUT',
-                headers: {
-                    'Content-type': 'application/json'
-                },
-                body: JSON.stringify(pensamento)
-            });
-            return await response.json();
+            const response = await axios.put(`${URL_BASE}/pensamentos/${pensamento.id}`, pensamento);
+            return await response.data;
         }
         catch{
             alert('Erro ao editar pensamento');
@@ -53,9 +47,7 @@ const api = {
 
     async excluirPensamento(id) {
         try{
-            const response = await fetch(`${URL_BASE}/pensamentos/${id}`, {
-                method: 'DELETE'
-            });
+            const response = await axios.delete(`${URL_BASE}/pensamentos/${id}`);
         }
         catch{
             alert('Erro ao excluir pensamento');
