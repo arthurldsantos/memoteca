8a4b5c4c02a7a7336351e4f99e2923f6ba7dd002
Arthur Santos <arthurldsantos@gmail.com>
2026-05-22 18:10:37 -0300

    criar funcao para filtrar pensamentos de acordo com o termo digitado pelo usuario

--- js/api.js
+++ js/api.js
@@ -53,6 +53,23 @@ const api = {
             alert('Erro ao excluir pensamento');
             throw error;
         }
+    },
+
+    async buscaPensamentosPorTermo(termo) {
+        try {
+            const pensamentos = await this.buscarPensamentos();
+
+            const termoEmMinusculas = termo.toLowerCase();
+
+            const pensamentosFiltrados = pensamentos.filter(pensamento => {
+                return(pensamento.conteudo.toLowerCase().includes(termoEmMinusculas) || 
+                        pensamento.autoria.conteudo.toLowerCase().includes(termoEmMinusculas));
+            });
+            return pensamentosFiltrados;
+        } catch (error) {
+            alert('Erro ao filtrar pensamentos');
+            throw error;
+        }
     }
 
 }
